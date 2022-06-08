d34c109d272b93472ed17bd54ac24fbda8bd2e98
Ayes <ayes2022@protonmail.com>
2022-06-09 00:27:23 +0300

    Added reddit, changed instances to farside

--- social-media-redirect.user.js
+++ social-media-redirect.user.js
@@ -7,8 +7,9 @@
 // @include     *://www.instagram.com/*
 // @include     *://youtube.com/*
 // @include     *://www.youtube.com/*
+// @include     *://reddit.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      ayes-web
 // @description Userscript that redirects social media sites to privacy alternatives
 // @homepageURL https://github.com/ayes-web/social-media-redirect/
@@ -17,22 +18,26 @@
 // ==/UserScript==
 
 // Enable/disable sites redirecting
-var redirect_twitter = true
-var redirect_youtube = true
-var redirect_instagram = true
+var redirect_twitter = true;
+var redirect_youtube = true;
+var redirect_instagram = true;
+var redirect_reddit = true;
 
 // Change instances
-var nitter_instance = "https://nitter.snopyta.org"
-var bibliogram_instance = "https://bibliogram.snopyta.org"
-var invidious_instance = "https://invidious.snopyta.org"
+var nitter_instance = "https://farside.link/nitter";
+var bibliogram_instance = "https://farside.link/bibliogram";
+var invidious_instance = "https://farside.link/invidious";
+var reddit_instance = "https://farside.link/libreddit";
 
 var site = window.location.hostname
 var path = window.location.pathname
 
 if (redirect_twitter && (site == "twitter.com" || site == "www.twitter.com")) {
-  window.location.replace(nitter_instance + window.location.pathname)
+  window.location.replace(nitter_instance + path)
 } else if (redirect_instagram && (site == "instagram.com" || site == "www.instagram.com")) {
-  window.location.replace(bibliogram_instance + window.location.pathname)
+  window.location.replace(bibliogram_instance + path)
 } else if (redirect_youtube && (site == "youtube.com" || site == "www.youtube.com")) {
-  window.location.replace(invidious_instance + window.location.pathname)
+  window.location.replace(invidious_instance + path)
+} else if (redirect_reddit && (site == "reddit.com")) {
+  window.location.replace(reddit_instance + path)
 }
